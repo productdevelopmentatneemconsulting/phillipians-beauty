b9dadcbce95e66984da6506f2720e7ccaae37478
Prakash Kumar <66732508+prakash-nc@users.noreply.github.com>
2020-08-10 11:52:51 +0530

    feat: add product details page with all the details (#12)
    * WIP: Ui related issue need to be fix
    
    * feat: add product details page with all the details
    
    * fix: build issue fixed

--- packages/beauty-portal-theme/src/components/Slider/index.tsx
+++ packages/beauty-portal-theme/src/components/Slider/index.tsx
@@ -231,7 +231,9 @@ const Slider: FunctionComponent<SliderInterface> = ({
                       alt={slide.image.alt}
                     />
                   </picture>
-                ) : null}
+                ) : (
+                  <span>Paras</span>
+                )}
               </figure>
             </div>
             <h3 className="bp-slider_caption">
