8cf735f2ec6710c1448d7f0ea719d3125bcc91df
Prakash Kumar <66732508+prakash-nc@users.noreply.github.com>
2020-08-11 10:59:22 +0530

    feat: add Author Grid type (#13)

---
+++ packages/beauty-portal-theme/src/components/AuthorGridStacker/models.ts
@@ -0,0 +1,9 @@
+export interface GridStackerInterface {
+  name: string;
+  slides: any;
+  headline: string | null;
+  searchCtaLabel: string | null;
+  searchTags?: string | null;
+  description?: string;
+  author?: any;
+}
