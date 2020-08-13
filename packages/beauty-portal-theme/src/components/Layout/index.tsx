c2db6cfaee506fcbe03ea12a43b664e35ee03690
vivekneem <VivekDesai@neemconsulting.com>
2020-08-13 12:18:03 +0530

    made subscription popup using scss & handle it with localstorage including time delay

--- packages/beauty-portal-theme/src/components/Layout/index.tsx
+++ packages/beauty-portal-theme/src/components/Layout/index.tsx
@@ -3,15 +3,19 @@ import classNames from 'classnames';
 import Header from '../Header';
 import Footer from '../Footer';
 import PageSchema from '../PageSchema';
+import SubscriptionPopup from '../SubscriptionPopup';
+import { MyContext } from '../../context/Context';
 import './styles.scss';
 
 const Layout = ({ className, children }: LayoutProps) => {
+  const [open, setOpen] = React.useContext(MyContext);
   return (
     <>
       <a href="#main" className="bp-skipLink">
         <span>Skip to content</span>
       </a>
       <Header />
+      <SubscriptionPopup open={open} setOpen={setOpen} />
       <PageSchema type={'WebSite'} />
       <main
         id="main"
