0f6d20797acf43e442413b267465144a0249b4f4
vivekneem <VivekDesai@neemconsulting.com>
2020-09-23 12:24:32 +0530

    added sticky related article on scroll for desktop devices

--- packages/studio/src/previews/TwitterCard/index.js
+++ packages/studio/src/previews/TwitterCard/index.js
@@ -1,22 +1,22 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
-import imageUrlBuilder from '@sanity/image-url';
+import React from 'react'
+import imageUrlBuilder from '@sanity/image-url'
 // eslint-disable-next-line import/no-unresolved
-import sanityClient from 'part:@sanity/base/client';
-import styles from './TwitterCard.css';
+import sanityClient from 'part:@sanity/base/client'
+import styles from './TwitterCard.css'
 
-const builder = imageUrlBuilder(sanityClient);
+const builder = imageUrlBuilder(sanityClient)
 
-const urlFor = (source) => {
-  return builder.image(source);
-};
+const urlFor = source => {
+  return builder.image(source)
+}
 
 const author = {
   name: 'Beautyportal',
   handle: 'Beautyportal',
   // insert your twiiter brand image url below
-  image: 'https://farm6.staticflickr.com/5510/14338202952_93595258ff_z.jpg',
-};
+  image: 'https://farm6.staticflickr.com/5510/14338202952_93595258ff_z.jpg'
+}
 
 const TwitterCard = ({ document, width }) => {
   const {
@@ -24,13 +24,12 @@ const TwitterCard = ({ document, width }) => {
     slug: { current: url },
     seo,
     subheading,
-    heroImage,
-  } = document.displayed;
-  const description = (seo && seo.metaDescription) || subheading;
-  const openGraphImage  =
-    (seo && seo.heroImage) || (heroImage && heroImage.image && heroImage.image);
+    heroImage
+  } = document.displayed
+  const description = (seo && seo.metaDescription) || subheading
+  const openGraphImage = (seo && seo.heroImage) || (heroImage && heroImage.image && heroImage.image)
 
-  const websiteUrlWithoutProtocol = url.split('://')[1];
+  const websiteUrlWithoutProtocol = url.split('://')[1]
 
   return (
     <div className={styles.seoItem}>
@@ -65,12 +64,12 @@ const TwitterCard = ({ document, width }) => {
         </a>
       </div>
     </div>
-  );
-};
+  )
+}
 
 TwitterCard.defaultProps = {
   document: null,
-  width: 500,
-};
+  width: 500
+}
 
-export default TwitterCard;
+export default TwitterCard
